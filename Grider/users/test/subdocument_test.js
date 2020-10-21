7b25a2af1351e2d306775ee5b1e9372d71a5c7b6
Gourav B <pit.wire@pm.me>
2020-10-21 02:13:09 -0400

    assert subdocument can be added to an existing user

--- Grider/users/test/subdocument_test.js
+++ Grider/users/test/subdocument_test.js
@@ -13,6 +13,26 @@ describe('Subdocuments', () => {
     .then((user) => {
         assert(user.posts[0].title === 'PostTitle');
         done();
-    })
-})
+    });
+  });
+
+    it('can add subdocuments to an existing user', (done) => {
+        const joe = new User({
+            name: 'Joe',
+            posts: []
+        });
+
+        joe.save()
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                user.posts.push({ title: 'New Post'})
+                return user.save();
+            })
+            .then(() => User.findOne({ name: 'Joe'}))
+            .then((user) => {
+                assert(user.posts[0].title === 'New Post');
+                done();
+           });
+        });
+        console.log("user can add a subdocument to an existing user");
 })
