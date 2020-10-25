c2f2d4f6c60806f1d457787272d38b4a9804f184
pit-dev <possibilitiesInTest@protonmail.com>
2020-10-25 17:16:21 -0400

    saving deeply nested association graph

--- Grider/users/test/association_test.js
+++ Grider/users/test/association_test.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
+const assert = require("assert");
 const User = require("../src/user");
 const Comment = require("../src/comment");
 const BlogPost = require("../src/blogPost");
 
-describe("Associations tests", () => {
+describe("Assocations", () => {
   let joe, blogPost, comment;
 
   beforeEach((done) => {
@@ -31,8 +32,34 @@ describe("Associations tests", () => {
     User.findOne({ name: "Joe" })
       .populate("blogPosts")
       .then((user) => {
-        console.log(user);
+        assert(user.blogPosts[0].title === "JS is Great");
         console.log("++++ 1. saves a relation between user and post");
+        done();
+      });
+  });
+
+  it("saves a full relation graph", (done) => {
+    User.findOne({ name: "Joe" })
+      .populate({
+        path: "blogPosts",
+        populate: {
+          path: "comments",
+          model: "comment",
+          populate: {
+            path: "user",
+            model: "user",
+          },
+        },
+      })
+      .then((user) => {
+        assert(user.name === "Joe");
+        assert(user.blogPosts[0].title === "JS is Great");
+        assert(
+          user.blogPosts[0].comments[0].content === "Congrats on great post"
+        );
+        assert(user.blogPosts[0].comments[0].user.name === "Joe");
+        console.log("++++ 2. saves a relation between user and post");
+
         done();
       });
   });
